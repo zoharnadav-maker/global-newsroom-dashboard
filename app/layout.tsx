48fb7a4a7704736c09c73d70c777bf5014d5f8a7
zoharnadav-maker <zohar.nadav@gmail.com>
2026-03-02 14:23:49 +0000

    Create RootLayout component for global dashboard

---
+++ app/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = { title: 'Global Newsroom Dashboard' };
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
