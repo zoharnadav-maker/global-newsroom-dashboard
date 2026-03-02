8858a9daa4f79346604e03a01c9ad8f7c9391fb9
zoharnadav-maker <zohar.nadav@gmail.com>
2026-03-02 14:24:45 +0000

    Add homepage

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>Global Newsroom Dashboard</h1>
+      <p>Powered by NewsAPI</p>
+      <p>Your unified news hub is coming soon...</p>
+    </div>
+  );
+}
